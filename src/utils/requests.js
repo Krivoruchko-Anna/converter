e804bc71eb0edfb8503e501825db3a0d6683fcb7
Anna <anna49.fog@yandex.ru>
2022-12-08 15:51:53 +0100

    feat: update Api key

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,47 +1,41 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://free.currconv.com/api/v7',
-    params: {
-        apiKey: '33ac2d9ba98418cfdf69'
-    }
+  baseURL: 'https://free.currconv.com/api/v7',
+  params: {apiKey: 'eb83db00cef855952a36'}
 });
 
 export const fetchData = async () => {
-    try {
-        const response = await axiosInstance.get(`/currencies`);
-        const results = response.data.results;
-        const list = Object.keys(results);
+  try {
+    const response = await axiosInstance.get(`/currencies`);
+    const results = response.data.results;
+    const list = Object.keys(results);
 
-        return list;
-    }
-
-    catch(e) {
-        return [];
-    }
+    return list;
+  } catch (e) {
+    return [];
+  }
 }
 
 export const fetchCurrency = async (firstCurrency, secondCurrency = 0) => {
-    try {
-        if (firstCurrency && secondCurrency) {
-            const response = await axiosInstance.get(`/convert?q=${firstCurrency}_${secondCurrency}&compact=y`);
-            const convertedResult = response.data[`${firstCurrency}_${secondCurrency}`];
-            const result = convertedResult.val;
+  try {
+    if (firstCurrency && secondCurrency) {
+      const response = await axiosInstance.get(`/convert?q=${firstCurrency}_${secondCurrency}&compact=y`);
+      const convertedResult = response.data[`${firstCurrency}_${secondCurrency}`];
+      const result = convertedResult.val;
 
-            return result;
-        }
+      return result;
     }
-
-    catch (e) {
-        return [];
-    }
+  } catch (e) {
+    return [];
+  }
 }
 
 export const fetchLocalCurrency = async (state) => {
-    const local = axios.get(`http://ip-api.com/json?fields=8413185`)
-        .then(response => state.selectedCurrency = response.data.currency);
+  const local = axios.get(`http://ip-api.com/json?fields=8413185`)
+    .then(response => state.selectedCurrency = response.data.currency);
 
-    return local;
+  return local;
 }
 
 
--- src/views/Converter.vue
+++ src/views/Converter.vue
@@ -2,39 +2,39 @@
   <div class="converter">
     <Form/>
     <div v-if="showOutcome" class="result">
-        <span class="result__span">
-            <b>{{ enteredSum }}</b> {{ firstCurrency }} &nbsp; = &nbsp;
-            <b>{{ (convertedCurrency * enteredSum).toFixed(2) }}</b> {{ secondCurrency }}
-        </span>
+      <span class="result__span">
+        <b>{{ enteredSum }}</b> {{ firstCurrency }} &nbsp; = &nbsp;
+        <b>{{ (convertedCurrency * enteredSum).toFixed(2) }}</b> {{ secondCurrency }}
+      </span>
     </div>
   </div>
 </template>
 
 <script>
 import Form from '@/components/Form.vue';
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'Converter',
   components: {
-      Form
+    Form
   },
-    computed: {
-      ...mapGetters(['enteredSum', 'firstCurrency', 'secondCurrency', 'showOutcome', 'convertedCurrency'])
-    }
- }
+  computed: {
+    ...mapGetters(['enteredSum', 'firstCurrency', 'secondCurrency', 'showOutcome', 'convertedCurrency'])
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .result {
-    font-size: 22px;
-    margin-top: 40px;
-    color: #6a7179;
+  font-size: 22px;
+  margin-top: 40px;
+  color: #6a7179;
 
-    &__span {
-        padding: 15px 30px;
-        border-radius: 4px;
-        box-shadow: rgb(0 17 51 / 15%) 0 3px 15px;
-    }
+  &__span {
+    padding: 15px 30px;
+    border-radius: 4px;
+    box-shadow: rgb(0 17 51 / 15%) 0 3px 15px;
+  }
 }
 </style>
